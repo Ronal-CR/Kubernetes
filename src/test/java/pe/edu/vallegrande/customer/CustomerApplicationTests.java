f972bc0dbc46fde1a539db4c0e113b8a1db31362
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2025-07-06 19:26:33 -0500

    kubernetes basic

---
+++ src/test/java/pe/edu/vallegrande/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
